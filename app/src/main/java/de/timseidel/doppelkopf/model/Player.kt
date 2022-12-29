f1c4d58946be3b126aff1770a9d3deba5760e90d
Tim Seidel <account@timseidel.de>
2022-12-29 12:19:00 +0100

    Made player a data class

--- app/src/main/java/de/timseidel/doppelkopf/model/Player.kt
+++ app/src/main/java/de/timseidel/doppelkopf/model/Player.kt
@@ -1,5 +1,3 @@
 package de.timseidel.doppelkopf.model
 
-import de.timseidel.doppelkopf.util.IdGenerator
-
-class Player(val id: String, var name: String)
+data class Player(val id: String, var name: String)

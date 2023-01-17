03d33b9afb519eca06c3dec250758378917a38f1
Tim Seidel <account@timseidel.de>
2023-01-17 16:26:02 +0100

    Optional csv exporter for game histories (To android.log)

---
+++ app/src/main/java/de/timseidel/doppelkopf/export/CSVGameHistoryExporter.kt
@@ -0,0 +1,72 @@
+package de.timseidel.doppelkopf.export
+
+import de.timseidel.doppelkopf.model.Faction
+import de.timseidel.doppelkopf.model.Game
+import de.timseidel.doppelkopf.util.DokoUtil
+
+class CSVGameHistoryExporter {
+
+    fun exportGameHistory(games: List<Game>): String {
+        if (games.isEmpty()) {
+            return ""
+        }
+
+        val csv = StringBuilder()
+
+        val players = games.first().players
+        players.forEach { p ->
+            csv.append(p.player.name)
+            csv.append(";")
+        }
+        csv.append("\n")
+
+        games.forEach { g ->
+            g.players.forEach { p ->
+                val result = DokoUtil.getPlayerResult(p.player, g)
+                if (result.faction != Faction.NONE) {
+                    csv.append(result.tacken.toString())
+                }
+                csv.append(";")
+            }
+
+            if (g.isBockrunde) csv.append("x")
+            csv.append("\n")
+        }
+
+        return csv.toString()
+    }
+
+    fun exportGameHistoryCumulative(games: List<Game>): String {
+        if (games.isEmpty()) {
+            return ""
+        }
+
+        val csv = StringBuilder()
+
+        val players = games.first().players
+        players.forEach { p ->
+            csv.append(p.player.name)
+            csv.append(";")
+        }
+        csv.append("\n")
+
+        val tacken = MutableList(players.size) { 0 }
+
+        games.forEach { g ->
+            g.players.forEachIndexed { i, p ->
+                val result = DokoUtil.getPlayerResult(p.player, g)
+                tacken[i] += result.tacken
+                if (result.faction != Faction.NONE) {
+                    csv.append(tacken[i].toString())
+                }
+
+                csv.append(";")
+            }
+
+            if (g.isBockrunde) csv.append("x")
+            csv.append("\n")
+        }
+
+        return csv.toString()
+    }
+}

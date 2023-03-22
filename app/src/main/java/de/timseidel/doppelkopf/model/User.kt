8306a04b49b11bfef184e5d11fcb08171dc611e3
Tim Seidel <account@timseidel.de>
2023-03-22 14:32:05 +0100

    Removed unused file

--- app/src/main/java/de/timseidel/doppelkopf/model/User.kt
+++
@@ -1,8 +0,0 @@
-package de.timseidel.doppelkopf.model
-
-import de.timseidel.doppelkopf.util.IdGenerator
-
-class User(
-    val id: String = IdGenerator.generateIdWithTimestamp("user"),
-    val groupIds: List<String> //TODO: Maybe schon Gruppenobjekte (ohne 'deep session')
-)
